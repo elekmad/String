0bf50c5530a90f5be00a8fb1df26985839eb03d4
elekmad <damien.warin@gmail.com>
2016-11-23 19:42:44 +0100

    Utilisation de '' pour bien délimiter l'affichage Utilisation de la nouvelle possibilité de compatibilité char* du get_data.

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     String_append_char_string(&test, "foobar");
     char *ret;
     String_compute_char_string(&test, &ret);
-    printf("%.*s %s\n", (int)String_get_length(&test), String_get_data(&test), ret);
+    printf("'%.*s' '%s' '%s'\n", (int)String_get_length(&test), String_get_data(&test), ret, String_get_data(&test));
     free(ret);
     exit(0);
 }
