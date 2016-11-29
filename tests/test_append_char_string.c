f01acf8b012ae4f06ccb46b1c410bb9ce57e2aa0
elekmad <damien.warin@gmail.com>
2016-11-29 18:57:03 +0100

    Utilisation de CTest pourles tests unitaires, et premiers tests ajoutés.

---
+++ tests/test_append_char_string.c
@@ -0,0 +1,30 @@
+#include <String.h>
+#include <string.h>
+#include <stdio.h>
+
+#define CHAR_STRING_TEST "foobar"
+
+int main(int argc, char **argv)
+{
+    int ret = 0;
+    String s;
+    String_init(&s);
+    String_append_char_string(&s, CHAR_STRING_TEST);
+    if(String_get_length(&s) != strlen(CHAR_STRING_TEST))
+    {
+        ret = -1;
+        fprintf(stderr, "Lengths does not match : %zu <> %zu\n", String_get_length(&s), strlen(CHAR_STRING_TEST));
+    }
+    else if(String_get_data(&s) == NULL)
+    {
+        ret = -1;
+        fprintf(stderr, "data is null\n");
+    }
+    else if(strcmp(String_get_char_string(&s), CHAR_STRING_TEST) != 0)
+    {
+        ret = -1;
+        fprintf(stderr, "char string does not match : '%s' <> '%s'\n", String_get_char_string(&s), CHAR_STRING_TEST);
+    }
+    String_finalize(&s);
+    return ret;
+}
---
+++ tests/test_init.c
@@ -0,0 +1,27 @@
+#include <String.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int ret = 0;
+    String s;
+    String_init(&s);
+    if(String_get_length(&s) != 0)
+    {
+        ret = -1;
+        fprintf(stderr, "Lengths is not 0 : %zu\n", String_get_length(&s));
+    }
+    else if(String_get_data(&s) != NULL)
+    {
+        ret = -1;
+        fprintf(stderr, "data is not null\n");
+    }
+    else if(strcmp(String_get_char_string(&s), "") != 0)
+    {
+        ret = -1;
+        fprintf(stderr, "char string should be empty : '%s'\n", String_get_char_string(&s));
+    }
+    String_finalize(&s);
+    return ret;
+}
